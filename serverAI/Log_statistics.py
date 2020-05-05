35ac0f8fb5f66e9c54e8b3ef1e904e92eefe0e3c
rbarrass <raphael.barrasset@gmail.com>
2020-05-05 18:09:00 +0200

    Stats : Calcul de la moyennne et de la médianeé

--- serverAI/Log_statistics.py
+++ serverAI/Log_statistics.py
@@ -1,6 +1,5 @@
 from datetime import datetime, date
-from time import strftime
-
+from statistics import median
 import pymysql
 
 months31 = (1, 3, 5, 7, 8, 10, 12)
@@ -38,6 +37,7 @@ def next_day(my_date):
     new_date = date(my_year, my_month, my_day)
     return new_date
 
+
 def count_day_rows(my_date):
     # Open database connection
     db = pymysql.connect("localhost", "admin", "admin", "smartwifi")
@@ -46,8 +46,8 @@ def count_day_rows(my_date):
     cursor.execute("SELECT COUNT(*) FROM csv_data WHERE time REGEXP \'" + str(my_date) + " *\'")
     rows = [my_date, cursor.fetchone()[0]]
     db.close()
-
     return rows
+
 
 def count_days_rows(date_min, date_max):
     days_rows = []
@@ -58,8 +58,31 @@ def count_days_rows(date_min, date_max):
     return days_rows
 
 
+def average_log_rows(days_rows):
+    all_rows = 0
+    for day_rows in days_rows:
+        all_rows += day_rows[1]
+    # Averaging
+    rows_average = all_rows / len(days_rows)
+    return int(rows_average)
+
+
+def median_log_rows(days_rows):
+    rows_array = []
+    for day_rows in days_rows:
+        rows_array.append(day_rows[1])
+    # Median calculation
+    rows_median = median(rows_array)
+    return rows_median
+
+
 #print(count_day_rows(date(2018, 3, 7)))
 #print(date(2018, 3, 7))
 #print(next_day(date(2021, 2, 28)))
 
-print(count_days_rows(date(2018, 3, 7), date(2018, 3, 15)))
+test_days_rows = count_days_rows(date(2018, 3, 7), date(2018, 3, 15))
+print(test_days_rows)
+average = average_log_rows(test_days_rows)
+print("Moyenne : " + str(average))
+median = median_log_rows(test_days_rows)
+print("Médiane : " + str(median))
