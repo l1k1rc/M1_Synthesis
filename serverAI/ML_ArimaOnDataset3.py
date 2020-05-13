ff137abcafb051a2272f3f4f3b63986b3023bd0d
l1k1 <throwaraccoon@gmail.com>
2020-05-13 14:59:32 +0200

    arima modif

--- serverAI/ML_ArimaOnDataset3.py
+++ serverAI/ML_ArimaOnDataset3.py
@@ -14,8 +14,8 @@ from statsmodels.tsa.arima_model import ARIMA
 import csv
 from statsmodels.tsa.stattools import adfuller # to study a time serie
 
-
-'''mydb = mysql.connector.connect(
+# Connect to database and send a request to find number of client per minutes
+mydb = mysql.connector.connect(
     host="localhost",
     user="l1k1",
     passwd="raccoon",
@@ -24,7 +24,7 @@ from statsmodels.tsa.stattools import adfuller # to study a time serie
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT time, count(*) as NUM FROM csv_data GROUP BY time LIMIT 200")
+mycursor.execute("SELECT time, count(*) as NUM FROM csv_data GROUP BY time LIMIT 100")
 result_set = mycursor.fetchall()
 time = []
 user = []
@@ -37,19 +37,11 @@ for row in result_set:
 
 dttime = pd.DataFrame(dt)
 dttime.to_csv('../data/new_.csv', index=False, header=True)
-
-# register_matplotlib_converters()
-list_daf = []
-df = pd.read_csv('../data/new_.csv', parse_dates=['0'], index_col=['0'])'''
-'''for val in daf['1']:
-    list_daf.append(val)
-
-ts = list_daf
-print(ts)'''
+# convert the result into csv
 
 
 plt.rcParams.update({'figure.figsize': (9, 7), 'figure.dpi': 250})
-
+############ Make the serie stationnary ############
 def saveAndConvert(data,name='new_3'):
     list = pd.DataFrame(data)
     list.to_csv('../data/new_3.csv', index=False, header=True)
@@ -81,14 +73,17 @@ def obtainExploitableData(data):
 # Import data
 #df = pd.read_csv('../data/new_2.csv', names=['value'], header=0)
 df = pd.read_csv('../data/new_.csv', parse_dates=['0'], index_col=['0'])
+#Convert the data into an exploitable stationary serie
 new_df = obtainExploitableData(df['1'].tolist())
 df = saveAndConvert(new_df)
+############ Serie stationary ############
 
-##########################################################################################
-
+#If p-value is low, the serie is stationary
 result = adfuller(df.value.dropna())
 print('ADF Statistic: %f' % result[0])
 print('p-value: %f' % result[1])
+
+'''
 # Original Series
 fig, axes = plt.subplots(3, 2)
 axes[0, 0].plot(df.value)
@@ -106,7 +101,6 @@ axes[2, 0].set_title('2nd Order Differencing')
 plot_acf(df.value.diff().diff().dropna(), ax=axes[2, 1])
 
 plt.show()
-plt.rcParams.update({'figure.figsize': (9, 3), 'figure.dpi': 120})
 
 fig, axes = plt.subplots(1, 2)
 axes[0].plot(df.value.diff());
@@ -123,10 +117,10 @@ axes[1].set(ylim=(0, 1.2))
 plot_acf(df.value.diff().dropna(), ax=axes[1])
 
 plt.show()
-# => d =1 et p=1
+# => d =1 et p=1'''
 
-# 1,1,1 ARIMA Model
-model = ARIMA(df.value, order=(3, 0, 2))
+# 1,1,1 ARIMA Model as q,d,p
+model = ARIMA(df.value, order=(6, 2, 3))
 model_fit = model.fit(disp=0)
 print(model_fit.summary())
 
@@ -134,19 +128,23 @@ print(model_fit.summary())
 model_fit.plot_predict(dynamic=False)
 plt.show()
 
-from statsmodels.tsa.stattools import acf
+'''
 
+
+@:train : 60% of total values
+@:test : rest of train => 40% of total values
+''''''
 # Create Training and Test
-train = df.value[:160]
-test = df.value[160:]
+train = df.value[:85]
+test = df.value[85:]
 
 # Build Model
 # model = ARIMA(train, order=(3,2,1))
-model = ARIMA(train, order=(3, 0, 2))
+model = ARIMA(train, order=(3, 0, 2))#######################################################################################################
 fitted = model.fit(disp=0)
 
 # Forecast
-fc, se, conf = fitted.forecast(39, alpha=0.05)  # 95% conf => niveau de prédiction reste 36 cas par rapport au [:XX]
+fc, se, conf = fitted.forecast(15, alpha=0.05)  # 95% conf => niveau de prédiction reste 36 cas par rapport au [:XX]
 
 # Make as pandas series
 fc_series = pd.Series(fc, index=test.index)
@@ -165,12 +163,12 @@ plt.legend(loc='upper left', fontsize=8)
 plt.show()
 
 # Build Model
-model = ARIMA(train, order=(1, 0, 1))
+model = ARIMA(train, order=(1, 0, 1))#######################################################################################################
 fitted = model.fit(disp=-1)
 print(fitted.summary())
 
 # Forecast
-fc, se, conf = fitted.forecast(39, alpha=0.05)  # 95% conf
+fc, se, conf = fitted.forecast(15, alpha=0.05)  # 95% conf
 
 # Make as pandas series
 fc_series = pd.Series(fc, index=test.index)
@@ -186,4 +184,4 @@ plt.fill_between(lower_series.index, lower_series, upper_series,
                  color='k', alpha=.15)
 plt.title('Forecast vs Actuals')
 plt.legend(loc='upper left', fontsize=8)
-plt.show()
+plt.show()'''
