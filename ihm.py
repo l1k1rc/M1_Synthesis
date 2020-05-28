89c49accfc4b9dea3426e73199f965fb59350d7b
CASTELAINJulien <35867382+CASTELAINJulien@users.noreply.github.com>
2020-05-28 16:45:44 +0200

    Ajout de la zone de logs et insertion d'une image
    A continuer...

--- ihm.py
+++ ihm.py
@@ -1,6 +1,7 @@
 # On importe Tkinter
 from tkinter import *
 import tkinter as tk
+from PIL import Image, ImageTk
 
 #def du bouton stats
 def boutonFourreTout():
@@ -30,11 +31,16 @@ s_pers = Spinbox(Frame1, from_=0, to=500)
 Frame2 = Frame(fenetre, borderwidth=2, relief=GROOVE)
 Frame2.pack(side=BOTTOM, fill=X, ipady=50)
 
+
 Frame3 = Frame(fenetre, borderwidth=2,bg='white')
 Frame3.pack(fill=X, ipady=250)
 
+text = Text(Frame2)
+
 stats = Button(Frame1, text="Statistiques", fg="white", bg="green", command = boutonFourreTout)
 
+logs="blablablablablabla"+"\n"+"blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n""blablablablablabla"+"\n"
+logs = logs + "test" + "\n"
 Label(Frame1, text="Paramètres Modifiables", font=("Helvetica", 16)).pack()
 Label(Frame1, text="").pack()
 Label(Frame1, text="Nombre de bornes (de 1 à 10):").pack()
@@ -46,7 +52,27 @@ Label(Frame1, text="").pack()
 Label(Frame1, text="").pack()
 stats.pack()
 Label(Frame2, text="Logs en direct").pack(side=TOP, padx=80, pady=20)
+
+canvasLog = Canvas(Frame2, bg="Black", width=800, height=150)
+canvasLog.pack()
+scrollbar = Scrollbar(canvasLog, orient=VERTICAL, command=canvasLog.yview)
+scrollbar.place(relx=1, rely=0, relheight=1, anchor=NE)
+canvasLog.create_text(100,10,fill="white",font="Times 15",
+                        text=logs)
+scrollbar.config(command=canvasLog.yview)
+canvasLog.config(yscrollcommand=scrollbar.set)
+
 Label(Frame3, text="Simulation").pack(side=TOP, padx=80, pady=20)
+
+img = ImageTk.PhotoImage(file="user.png")
+canvasUser = tk.Canvas(Frame3, width=20, height=20)
+canvasUser.create_image(0, 0, anchor=tk.NW, image=img)
+canvasUser.pack()
+
+labelImg = tk.Label(canvasUser, image=img)
+labelImg.pack()
+
+
 
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
